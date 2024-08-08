e4050cc0837d33e1f950392f16920a0945e3433e
alinjfz <alinajafzadeh1970@gmail.com>
2024-08-08 16:15:38 +0330

    ollama is 502 error, need to manage chats

--- slm_back/app/__init__.py
+++ slm_back/app/__init__.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
     app.register_blueprint(main)
+    CORS(app)
     return app
+
+
+    # CORS(app, resources={r"/api/*": {"origins": "http://localhost:3001"}}, supports_credentials=True)
+
--- slm_back/app/routes.py
+++ slm_back/app/routes.py
@@ -1,17 +1,77 @@
 from flask import Blueprint, request, jsonify
+import requests
 import json
 import os
 
 main = Blueprint('main', __name__)
 
-CONVERSATIONS_FILE = os.path.join('back', 'static', 'conversations.json')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONVERSATIONS_FILE = os.path.join(BASE_DIR, 'static', 'conversations.json')
+
+EXTERNAL_API_URL = "http://localhost:11434/api/chat"
+
+import openai
+import logging
+
+logging.basicConfig(level=logging.ERROR)
+
+def call_llama3_openai_api(user_message):
+    try:
+        client = openai.OpenAI(
+        base_url="http://localhost:11434/api/chat",
+        api_key="nokeyneeded",
+        )
+        response = client.chat.completions.create(
+            model="llama3.1:8b",
+            temperature=0.7,
+            n=1,
+            messages=[{"role": "user", "content": user_message}]
+        )
+        return response
+    # except openai.error.OpenAIError as e:
+    #     logging.error(f"OpenAI API error: {e}")
+    #     # Handle specific errors or re-raise if necessary
+    #     raise
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+        raise
+
+
+def call_llama3_api(prompt):
+    data = {
+        "model": "llama3.1:8b",
+        "messages": [
+            {
+              "role": "user",
+              "content": prompt
+            }
+        ],
+        "stream": False
+    }
+    
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(EXTERNAL_API_URL, headers=headers, json=data)  
+    print(response)
+    return 
+    # return response.json()
+    # return response.json()#['message']['content']
+    from langchain_community.chat_models import ChatOllama
+
+    llm = ChatOllama(model="llama3", temperature=0)
+    response = llm.invoke("who wrote the book godfather?")
+    print(response.content)
 
 @main.route('/api/chat', methods=['POST'])
 def chat():
     data = request.json
-    # Handle chat logic here (e.g., call an AI model)
-    response = {"message": f"Echo: {data['message']}"}
-    return jsonify(response)
+    user_message = data['message']
+    
+    # Call the external API
+    bot_response = call_llama3_api(user_message)
+    # bot_response = call_llama3_openai_api(user_message)
+    return jsonify({"message": bot_response})
 
 @main.route('/api/conversations', methods=['GET'])
 def get_conversations():
