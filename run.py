3645c765329f3ed3bc945ae23d4225eb915ec026
alinjfz <alinajafzadeh1970@gmail.com>
2024-08-09 21:39:28 +0330

    run.py updated to use ollama in it

--- run.py
+++ run.py
@@ -7,6 +7,10 @@ def run_flask():
 def run_react():
     subprocess.Popen(['npm', 'start'], cwd='slm_front')
 
+def run_llama():
+    subprocess.Popen(['ollama', 'run', 'llama3.1:8b'])
+
 if __name__ == '__main__':
+    run_llama()
     run_react()
     run_flask()
